72fca9ab58433376769d2f8bd2f78683e08a6d56
Rovelt123 <78000528+Rovelt123@users.noreply.github.com>
2026-08-27 22:30:18 +0200

    Add Javalin health API

--- backend/src/main/java/Main.java
+++ backend/src/main/java/Main.java
@@ -1,5 +1,12 @@
+import io.javalin.Javalin;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Backend is ready.");
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "7070"));
+
+        Javalin app = Javalin.create()
+                .get("/api/health", ctx -> ctx.result("API OK"));
+
+        app.start(port);
     }
 }
