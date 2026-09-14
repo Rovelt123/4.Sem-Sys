6801d87f1d804b7fc63eb6b022ddcb3bbe7d0ad6
Tesselation93 <tessvanslyckspam@gmail.com>
2026-09-14 15:23:34 +0200

    Check password length, case and special character on the register form

--- frontend/src/pages/register/RegisterPage.jsx
+++ frontend/src/pages/register/RegisterPage.jsx
@@ -19,6 +19,28 @@ function parseErrorMessage(text) {
 
 // ________________________________________________________
 
+function validatePassword(password) {
+  if (password.length < 8 || password.length > 30) {
+    return 'The password must be between 8 and 30 characters'
+  }
+
+  if (password.search(/[a-z]/) < 0) {
+    return 'The password must contain a lowercase letter'
+  }
+
+  if (password.search(/[A-Z]/) < 0) {
+    return 'The password must contain an uppercase letter'
+  }
+
+  if (password.search(/[^A-Za-z0-9]/) < 0) {
+    return 'The password must contain a special character'
+  }
+
+  return ''
+}
+
+// ________________________________________________________
+
 function RegisterPage({ onRegisterSuccess }) {
   const [form, setForm] = useState({
     first_name: '',
@@ -41,6 +63,13 @@ function RegisterPage({ onRegisterSuccess }) {
   const handleSubmit = async (e) => {
     e.preventDefault()
     setError('')
+
+    const passwordError = validatePassword(form.password)
+
+    if (passwordError) {
+      setError(passwordError)
+      return
+    }
 
     if (form.password !== form.repeat_password) {
       setError('The passwords do not match')
@@ -130,12 +159,14 @@ function RegisterPage({ onRegisterSuccess }) {
           required
         />
 
-        <label htmlFor="password">Password</label>
+        <label htmlFor="password">Password (8-30 characters, upper and lower case, one special character)</label>
         <input
           id="password"
           name="password"
           type="password"
           value={form.password}
+          minLength={8}
+          maxLength={30}
           onChange={handleChange}
           required
         />
@@ -146,6 +177,7 @@ function RegisterPage({ onRegisterSuccess }) {
           name="repeat_password"
           type="password"
           value={form.repeat_password}
+          maxLength={30}
           onChange={handleChange}
           required
         />
