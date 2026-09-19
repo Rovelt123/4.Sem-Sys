16462b6eb67f8d95319068e42ff8463d532a3c1d
Rovelt123 <78000528+Rovelt123@users.noreply.github.com>
2026-09-19 15:25:40 +0200

    Fix frontend error.

--- frontend/src/pages/createwedding/CreateWeddingPage.jsx
+++ frontend/src/pages/createwedding/CreateWeddingPage.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router'
 import { getToken } from '../../utils/storage'
 import styles from './CreateWeddingPage.module.css'
 
-const API_BASE = import.meta.env.VITE_API_URL ?? 'http://localhost:9292/api'
+const API_BASE = 'http://sys2.roneu.dk/api' ?? 'http://localhost:9292/api'
 
 // ________________________________________________________
 
